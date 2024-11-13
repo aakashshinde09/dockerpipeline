cbebd872d15974f6787aaf0a11673ff7ae19b231
Akash Shinde <saakashs1999@gmail.com>
2024-11-13 18:42:04 +0530

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-From nginx
+FROM nginx
 COPY . /opt
 WORKDIR /opt/angular-frontend
 RUN apt update
--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,4 +3,4 @@ set -e
 
 docker pull 010928179293.dkr.ecr.us-east-1.amazonaws.com/dockerapp:latest
 
-docker run -d -p 80:80 010928179293.dkr.ecr.us-east-1.amazonaws.com/dockerapp:latest
+docker run --name myapp -d -p 80:80 010928179293.dkr.ecr.us-east-1.amazonaws.com/dockerapp:latest
